36412f53fbac0cda6e3c6bf80c07cb292a9eeb32
Esraa Alsharit <47732120+EsraaAlsharit@users.noreply.github.com>
2026-03-22 21:39:48 +0300

    update

---
+++ GAME PROGRAMMING-PREBOOTCAMP/Game Fundamentals/Collections/Lists Exercise/mycode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<int> num = new List<int>([1, 2, 3, 4, 5]);
+
+        foreach (int res in num)
+        {
+            Console.WriteLine("the number is " + res + " the result is " + (res * 5));
+        }
+
+    }
+}
---
+++ GAME PROGRAMMING-PREBOOTCAMP/Game Fundamentals/Collections/Lists Exercise/readme.md
@@ -0,0 +1,2 @@
+Write a C# Sharp program to create a list of integers where each integer value is multiplied by 5.
+Print out the list of items after the multiplication.
