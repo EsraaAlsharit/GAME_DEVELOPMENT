fc641827e358738a5aa1e5625f271d85e767a153
Esraa Alsharit <47732120+EsraaAlsharit@users.noreply.github.com>
2026-03-22 22:20:47 +0300

    Lists Exercise

---
+++ GAME PROGRAMMING-PREBOOTCAMP/Game Fundamentals/Collections/Lists Exercise 2/Readme.md
@@ -0,0 +1,8 @@
+Write a C# program that reads a list of integers from the user and then prints out the sum of all the even numbers in the list.
+
+Here is an example of how the program should behave:
+
+```
+Enter a list of integers: 1 2 3 4 5 6 7 8 9
+The sum of all the even numbers in the list is 20
+```
---
+++ GAME PROGRAMMING-PREBOOTCAMP/Game Fundamentals/Collections/Lists Exercise 2/mycode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<int> num = new List<int>();
+        int res = 0;
+        Console.Write("Enter a list of integers: ");
+
+        for (int i = 0; i < 9; i++)
+        {
+            num.Add(int.Parse(Console.ReadLine()));
+        }
+        foreach (int item in num)
+        {
+            if(item%2==0)
+            res +=item;
+        }
+        Console.WriteLine("The sum of all the even numbers in the list is " + res);
+
+    }
+}
